f37f911d70e95627a2c530b599501b50f71b6564
hyungyeom <rlagusrua3687@gmail.com>
2024-01-13 15:26:28 +0900

    Feat : OAuth2AuthenticationSuccessHandler, OAuth2Controller 구현
    소셜 로그인 성공 시 토큰 생성 및 사용자 정보 전달하는 핸들러 작성
    
    Related: #10

---
+++ src/main/java/com/example/dgbackend/global/security/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,94 @@
+package com.example.dgbackend.global.security.oauth2.handler;
+
+import com.example.dgbackend.domain.enums.SocialType;
+import com.example.dgbackend.domain.member.Member;
+import com.example.dgbackend.domain.member.repository.MemberRepository;
+import com.example.dgbackend.global.jwt.JwtProvider;
+import com.example.dgbackend.global.security.oauth2.dto.response.AuthResponse;
+import com.example.dgbackend.global.security.oauth2.principal.CustomOAuth2User;
+import com.example.dgbackend.global.security.oauth2.userInfo.KakaoUserInfo;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    // TODO: 추후 프론트 주소로 변경
+    private final String frontendRedirectUrl = "https://localhost:8080";
+    private final MemberRepository memberRepository;
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+
+        SocialType socialType = customOAuth2User.getMember().getSocialType();
+        String email = null;
+
+        if (socialType.equals(SocialType.KAKAO)) {
+            KakaoUserInfo kakaoUserInfo = new KakaoUserInfo(customOAuth2User.getAttributes());
+            email = kakaoUserInfo.getEmail();
+        } else if (socialType.equals(SocialType.NAVER)) {
+            return;
+        } else if (socialType.equals(SocialType.APPLE)) {
+            return;
+        }
+
+        log.info(frontendRedirectUrl + "----------------------------- frontendRedirectUrl");
+
+        Member loginMember = memberRepository.findByEmailAndSocialType(email, socialType).get();
+        // 인증이 성공했을 때, Access Token과 Refresh Token 발급
+        registerHeaderToken(response, loginMember.getEmail());
+
+        // 리프레시 토큰을 Redis에 저장
+
+        AuthResponse authResponse = AuthResponse.builder()
+                .memberId(loginMember.getId())
+                .email(email)
+                .nickName(loginMember.getNickName())
+                .profileImageUrl(loginMember.getProfileImageUrl())
+                .socialType(String.valueOf(loginMember.getSocialType()))
+                .build();
+
+        registerResponse(response, authResponse);
+    }
+
+    // Header에 Access Token 담아서 전달
+    private void registerHeaderToken(HttpServletResponse response, String loginMemberEmail) {
+        String token = jwtProvider.generateAccessToken(loginMemberEmail);
+
+        response.setHeader("Authorization", token);
+        log.info("Token = " + token);
+    }
+
+    // 로그인에 성공한 사용자에게 발급된 인증 정보를 사용하여 프론트엔드로 리다이렉트
+    private void registerResponse(HttpServletResponse response, AuthResponse authResponse) throws IOException {
+
+        String encodedMemberId = URLEncoder.encode(String.valueOf(authResponse.getMemberId()), StandardCharsets.UTF_8);
+        String encodedEmail = URLEncoder.encode(authResponse.getEmail(), StandardCharsets.UTF_8);
+        String encodedNickName = URLEncoder.encode(authResponse.getNickName(), StandardCharsets.UTF_8);
+        String encodedProfileImageUrl = URLEncoder.encode(authResponse.getProfileImageUrl(), StandardCharsets.UTF_8);
+        String encodedSocialType = URLEncoder.encode(authResponse.getSocialType(), StandardCharsets.UTF_8);
+
+        // 프론트엔드 페이지로 토큰과 함께 리다이렉트
+        String RedirectUrl = String.format(
+                "%s/?memberId=%s&email=%s&nickName=%s&profileImageUrl=%s&socialType=%s",
+                frontendRedirectUrl, encodedMemberId, encodedEmail, encodedNickName,
+                encodedProfileImageUrl, encodedSocialType
+        );
+        response.sendRedirect(RedirectUrl);
+    }
+
+}

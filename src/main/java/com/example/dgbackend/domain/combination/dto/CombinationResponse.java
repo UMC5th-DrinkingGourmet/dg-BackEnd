86f8a90bc93868a89759eee2a9f6c7f0a5241178
hyungyeom <rlagusrua3687@gmail.com>
2024-01-15 18:21:07 +0900

    feat: 오늘의 조합 상세 정보 조회 API
    Related: #23

---
+++ src/main/java/com/example/dgbackend/domain/combination/dto/CombinationResponse.java
@@ -0,0 +1,188 @@
+package com.example.dgbackend.domain.combination.dto;
+
+import com.example.dgbackend.domain.combination.domain.Combination;
+import com.example.dgbackend.domain.combinationcomment.domain.CombinationComment;
+import com.example.dgbackend.domain.combinationimage.CombinationImage;
+import com.example.dgbackend.domain.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public class CombinationResponse {
+
+    /**
+     * 오늘의 조합 홈 페이지 DTO
+     */
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class CombinationPreviewDTOList {
+        List<CombinationPreviewDTO> combinationList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class CombinationPreviewDTO {
+        String title;
+        String info;
+        String combinationImageUrl;
+        Long likeCount;
+        Long commentCount;
+    }
+
+    // Page<Combination> -> Page<CombinationPreviewDTO> 로 변환
+    public static CombinationPreviewDTOList toCombinationPreviewDTOList(Page<Combination> combinations) {
+
+        List<CombinationPreviewDTO> combinationPreviewDTOS = combinations.stream()
+                .map(CombinationResponse::toCombinationPreviewDTO)
+                .toList();
+
+        return CombinationPreviewDTOList.builder()
+                .combinationList(combinationPreviewDTOS)
+                .listSize(combinationPreviewDTOS.size())
+                .totalPage(combinations.getTotalPages())
+                .totalElements(combinations.getTotalElements())
+                .isFirst(combinations.isFirst())
+                .isLast(combinations.isLast())
+                .build();
+    }
+
+    // Combination -> CombinationPreviewDTO로 변환
+    public static CombinationPreviewDTO toCombinationPreviewDTO(Combination combination) {
+        // TODO: 대표 이지미 정하기
+        String imageUrl = combination.getCombinationImages()
+                .stream()
+                .findFirst()
+                .map(CombinationImage::getImageUrl)
+                .orElse(null);
+
+        return CombinationPreviewDTO.builder()
+                .title(combination.getTitle())
+                .info(combination.getInfo())
+                .combinationImageUrl(imageUrl)
+                .likeCount(combination.getLikeCount())
+                .commentCount(combination.getCommentCount())
+                .build();
+    }
+
+    /**
+     * 오늘의 조합 상세 정보 DTO
+     */
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class CombinationDetailDTO {
+        CombinationResult combinationResult;
+        MemberResult memberResult;
+        CombinationCommentResult combinationCommentResult;
+    }
+
+    public static CombinationDetailDTO toCombinationDetailDTO(CombinationResult combinationResult,
+                                                              MemberResult memberResult,
+                                                              CombinationCommentResult combinationCommentResult) {
+        return CombinationDetailDTO.builder()
+                .combinationResult(combinationResult)
+                .memberResult(memberResult)
+                .combinationCommentResult(combinationCommentResult)
+                .build();
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class CombinationResult {
+        Long combinationId;
+        String title;
+        String info;
+        String content;
+    }
+
+    public static CombinationResult toCombinationResult(Combination combination) {
+        return CombinationResult.builder()
+                .combinationId(combination.getId())
+                .title(combination.getTitle())
+                .info(combination.getInfo())
+                .content(combination.getContent())
+                .build();
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class MemberResult {
+        Long memberId;
+        String name;
+        String profileImageUrl;
+    }
+
+    public static MemberResult toMemberResult(Member member) {
+        return MemberResult.builder()
+                .memberId(member.getId())
+                .name(member.getName())
+                .profileImageUrl(member.getProfileImageUrl())
+                .build();
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class CombinationCommentResult {
+        List<CombinationCommentPreviewDTO> combinationCommentList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class CombinationCommentPreviewDTO {
+        Long combinationCommentId;
+        String content;
+        Long parentId;
+    }
+
+    public static CombinationCommentPreviewDTO toCommentPreviewDTO(CombinationComment comment) {
+        return CombinationCommentPreviewDTO.builder()
+                .combinationCommentId(comment.getId())
+                .content(comment.getContent())
+                .parentId(comment.getParentId())
+                .build();
+    }
+
+    public static CombinationCommentResult toCombinationCommentResult(Page<CombinationComment> comments) {
+
+        List<CombinationCommentPreviewDTO> commentPreviewDTOS = comments.stream()
+                .map(CombinationResponse::toCommentPreviewDTO)
+                .toList();
+
+        return CombinationCommentResult.builder()
+                .combinationCommentList(commentPreviewDTOS)
+                .listSize(commentPreviewDTOS.size())
+                .totalPage(comments.getTotalPages())
+                .totalElements(comments.getTotalElements())
+                .isFirst(comments.isFirst())
+                .isLast(comments.isLast())
+                .build();
+    }
+
+}
---
+++ src/main/java/com/example/dgbackend/domain/combination/service/CombinationQueryServiceImpl.java
@@ -0,0 +1,58 @@
+package com.example.dgbackend.domain.combination.service;
+
+import com.example.dgbackend.domain.combination.domain.Combination;
+import com.example.dgbackend.domain.combination.dto.CombinationResponse;
+import com.example.dgbackend.domain.combination.repository.CombinationRepository;
+import com.example.dgbackend.domain.combinationcomment.domain.CombinationComment;
+import com.example.dgbackend.domain.combinationcomment.service.CombinationCommentQueryService;
+import com.example.dgbackend.domain.member.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.example.dgbackend.domain.combination.dto.CombinationResponse.*;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CombinationQueryServiceImpl implements CombinationQueryService{
+
+    private final CombinationRepository combinationRepository;
+    private final CombinationCommentQueryService combinationCommentQueryService;
+
+    /*
+    오늘의 조합 홈 조회(페이징)
+     */
+    @Override
+    public CombinationPreviewDTOList getCombinationPreviewDTOList(Integer page) {
+        Page<Combination> combinations = combinationRepository.findAll(PageRequest.of(page, 10));
+        return toCombinationPreviewDTOList(combinations);
+    }
+
+    /**
+     * 오늘의 조합 상세 조회
+     * @param combinationId
+     */
+    @Override
+    public CombinationDetailDTO getCombinationDetailDTO(Long combinationId) {
+
+        // TODO: 예외 처리 - Combination
+        Combination combination = combinationRepository.findById(combinationId).get();
+
+        CombinationResult combinationResult = toCombinationResult(combination);
+
+        // Member
+        Member member = combination.getMember();
+        MemberResult memberResult = toMemberResult(member);
+
+        // CombinationComment
+        Page<CombinationComment> combinationComments =
+                combinationCommentQueryService.getCombinationCommentFromCombination(combination, PageRequest.of(0, 10));
+
+        CombinationCommentResult combinationCommentResult = toCombinationCommentResult(combinationComments);
+
+        return toCombinationDetailDTO(combinationResult, memberResult, combinationCommentResult);
+    }
+}

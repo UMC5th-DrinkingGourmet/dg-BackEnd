5e097ca619d4b7dc56071428f5cf2df102fabb84
hyungyeom <rlagusrua3687@gmail.com>
2024-01-18 22:15:31 +0900

    refactor: 오늘의 조합 작성 API 재구현
    이미지와 텍스트 데이터 분리하여 처리
    
    Related: #42

---
+++ src/main/java/com/example/dgbackend/domain/combinationimage/service/CombinationImageCommandService.java
@@ -0,0 +1,14 @@
+package com.example.dgbackend.domain.combinationimage.service;
+
+import com.example.dgbackend.domain.combination.domain.Combination;
+import com.example.dgbackend.domain.combinationimage.dto.CombinationImageResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface CombinationImageCommandService {
+
+    void updateCombinationImage(Combination combination, List<String> combinationImageList);
+
+    CombinationImageResponse.CombinationImageResult uploadImage(List<MultipartFile> multipartFiles);
+}

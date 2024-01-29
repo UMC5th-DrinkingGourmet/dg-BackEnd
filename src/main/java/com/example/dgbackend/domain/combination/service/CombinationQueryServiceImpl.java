8c16951128dc402bfd9749d89d1ad665a903e1eb
Minhyeok Song <minhyeok.dev@gmail.com>
2024-01-29 16:24:46 +0900

    Feat: #51 주간 베스트 조합 조회 API 구현
    - Controller, Service, Repository 구현
    - 좋아요 10개 이상 게시글 조회

--- src/main/java/com/example/dgbackend/domain/combination/service/CombinationQueryServiceImpl.java
+++ src/main/java/com/example/dgbackend/domain/combination/service/CombinationQueryServiceImpl.java
@@ -1,5 +1,15 @@
 package com.example.dgbackend.domain.combination.service;
 
+import static com.example.dgbackend.domain.combination.dto.CombinationResponse.CombinationDetailResult;
+import static com.example.dgbackend.domain.combination.dto.CombinationResponse.CombinationEditResult;
+import static com.example.dgbackend.domain.combination.dto.CombinationResponse.CombinationPreviewResultList;
+import static com.example.dgbackend.domain.combination.dto.CombinationResponse.CombinationResult;
+import static com.example.dgbackend.domain.combination.dto.CombinationResponse.toCombinationDetailResult;
+import static com.example.dgbackend.domain.combination.dto.CombinationResponse.toCombinationPreviewResultList;
+import static com.example.dgbackend.domain.combination.dto.CombinationResponse.toCombinationResult;
+import static com.example.dgbackend.domain.combinationcomment.dto.CombinationCommentResponse.CommentPreViewResult;
+import static com.example.dgbackend.domain.member.dto.MemberResponse.toMemberResult;
+
 import com.example.dgbackend.domain.combination.Combination;
 import com.example.dgbackend.domain.combination.dto.CombinationResponse;
 import com.example.dgbackend.domain.combination.repository.CombinationRepository;
@@ -11,17 +21,12 @@ import com.example.dgbackend.domain.member.Member;
 import com.example.dgbackend.domain.member.dto.MemberResponse;
 import com.example.dgbackend.global.common.response.code.status.ErrorStatus;
 import com.example.dgbackend.global.exception.ApiException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-import static com.example.dgbackend.domain.combination.dto.CombinationResponse.*;
-import static com.example.dgbackend.domain.combinationcomment.dto.CombinationCommentResponse.CommentPreViewResult;
-import static com.example.dgbackend.domain.member.dto.MemberResponse.toMemberResult;
 
 @Service
 @Transactional(readOnly = true)
@@ -41,8 +46,8 @@ public class CombinationQueryServiceImpl implements CombinationQueryService {
 
         List<Combination> combinationList = combinations.getContent();
         List<List<HashTagOption>> hashTagOptionList = combinationList.stream()
-                .map(hashTagOptionRepository::findAllByCombinationWithFetch)
-                .toList();
+            .map(hashTagOptionRepository::findAllByCombinationWithFetch)
+            .toList();
 
         return toCombinationPreviewResultList(combinations, hashTagOptionList);
     }
@@ -55,10 +60,11 @@ public class CombinationQueryServiceImpl implements CombinationQueryService {
 
         // Combination
         Combination combination = combinationRepository.findById(combinationId).orElseThrow(
-                () -> new ApiException(ErrorStatus._COMBINATION_NOT_FOUND)
+            () -> new ApiException(ErrorStatus._COMBINATION_NOT_FOUND)
         );
 
-        List<HashTagOption> hashTagOptions = hashTagOptionRepository.findAllByCombinationWithFetch(combination);
+        List<HashTagOption> hashTagOptions = hashTagOptionRepository.findAllByCombinationWithFetch(
+            combination);
         CombinationResult combinationResult = toCombinationResult(combination, hashTagOptions);
 
         // Member
@@ -66,7 +72,8 @@ public class CombinationQueryServiceImpl implements CombinationQueryService {
         MemberResponse.MemberResult memberResult = toMemberResult(member);
 
         // CombinationComment
-        CommentPreViewResult combinationCommentResult = combinationCommentQueryService.getCommentsFromCombination(combinationId, 0);
+        CommentPreViewResult combinationCommentResult = combinationCommentQueryService.getCommentsFromCombination(
+            combinationId, 0);
 
         return toCombinationDetailResult(combinationResult, memberResult, combinationCommentResult);
     }
@@ -78,19 +85,22 @@ public class CombinationQueryServiceImpl implements CombinationQueryService {
     public CombinationEditResult getCombinationEditResult(Long combinationId) {
 
         Combination combination = combinationRepository.findById(combinationId).orElseThrow(
-                () -> new ApiException(ErrorStatus._COMBINATION_NOT_FOUND)
+            () -> new ApiException(ErrorStatus._COMBINATION_NOT_FOUND)
         );
 
         List<CombinationImage> combinationImages = combination.getCombinationImages();
 
-        List<HashTagOption> hashTagOptions = hashTagOptionRepository.findAllByCombinationWithFetch(combination);
+        List<HashTagOption> hashTagOptions = hashTagOptionRepository.findAllByCombinationWithFetch(
+            combination);
 
-        return CombinationResponse.toCombinationEditResult(combination, hashTagOptions, combinationImages);
+        return CombinationResponse.toCombinationEditResult(combination, hashTagOptions,
+            combinationImages);
     }
 
     @Override
     public boolean existCombination(Long combinationId) {
         return combinationRepository.existsById(combinationId);
+    }
 
     /*
      * Combination 조회
@@ -98,7 +108,22 @@ public class CombinationQueryServiceImpl implements CombinationQueryService {
     @Override
     public Combination getCombination(Long combinationId) {
         return combinationRepository.findById(combinationId).orElseThrow(
-                () -> new ApiException(ErrorStatus._COMBINATION_NOT_FOUND)
+            () -> new ApiException(ErrorStatus._COMBINATION_NOT_FOUND)
         );
     }
+
+    @Override
+    public CombinationPreviewResultList getWeeklyBestCombinationPreviewResultList(Integer page) {
+        PageRequest pageRequest = PageRequest.of(page, 10);
+        Page<Combination> combinations = combinationRepository.findCombinationsByLikeCountGreaterThanEqualAndStateIsTrueOrderByCreatedAtDesc(
+            10L, pageRequest);
+
+        List<Combination> combinationList = combinations.getContent();
+        List<List<HashTagOption>> hashTagOptionList = combinationList.stream()
+            .map(hashTagOptionRepository::findAllByCombinationWithFetch)
+            .toList();
+
+        return toCombinationPreviewResultList(combinations, hashTagOptionList);
+    }
+
 }

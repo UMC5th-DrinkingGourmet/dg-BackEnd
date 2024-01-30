13a15bba04b8424481c1c07d5ac17813e412ea86
Minhyeok Song <minhyeok.dev@gmail.com>
2024-01-30 18:25:13 +0900

    Revert "Feat: Combination 엔티티 Like 관련 메서드 구현"
    This reverts commit 77530309fc9ab228ef223fdbd44b60e9da7f211d.

--- src/main/java/com/example/dgbackend/domain/combinationlike/CombinationLike.java
+++ src/main/java/com/example/dgbackend/domain/combinationlike/CombinationLike.java
@@ -3,13 +3,7 @@ package com.example.dgbackend.domain.combinationlike;
 import com.example.dgbackend.domain.combination.Combination;
 import com.example.dgbackend.domain.member.Member;
 import com.example.dgbackend.global.common.BaseTimeEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -35,23 +29,4 @@ public class CombinationLike extends BaseTimeEntity {
     @JoinColumn(name = "combination_id")
     private Combination combination;
 
-    public CombinationLike(Combination combination, Member member) {
-        this.combination = combination;
-        this.member = member;
-        this.combination.increaseLikeCount();
-    }
-
-    public CombinationLike changeState() {
-        this.state = !this.state;
-        if (!this.state) {
-            this.combination.decreaseLikeCount();
-        } else {
-            this.combination.increaseLikeCount();
-        }
-        return this;
-    }
-
-    public Boolean nowCombinationLikeState() {
-        return this.state;
-    }
 }

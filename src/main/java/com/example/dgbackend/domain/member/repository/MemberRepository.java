923e83700d9b793945b831d7460da7c8fab2d10d
hyungyeom <rlagusrua3687@gmail.com>
2024-02-03 22:25:25 +0900

    refactor: JwtProvider 만료 시간 및 파싱하는 데이터 수정
    Related: #10

--- src/main/java/com/example/dgbackend/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/dgbackend/domain/member/repository/MemberRepository.java
@@ -12,4 +12,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByName(String name);
 
     Boolean existsByProviderAndProviderId(String provider, String providerId);
+
+    Optional<Member> findByProviderAndProviderId(String provider, String providerId);
 }

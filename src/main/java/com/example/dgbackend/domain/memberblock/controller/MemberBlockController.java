cbf170affc03211f1250f79684f1d9788d24cc9e
SongMinHyeok <minhyeok.dev@gmail.com>
2024-05-20 22:27:11 +0900

    Feat: 차단 API 메서드 오류 수정
    Related: #184

--- src/main/java/com/example/dgbackend/domain/memberblock/controller/MemberBlockController.java
+++ src/main/java/com/example/dgbackend/domain/memberblock/controller/MemberBlockController.java
@@ -6,6 +6,9 @@ import com.example.dgbackend.domain.memberblock.dto.MemberBlockResponse;
 import com.example.dgbackend.domain.memberblock.service.MemberBlockServiceImpl;
 import com.example.dgbackend.global.common.response.ApiResponse;
 import com.example.dgbackend.global.jwt.annotation.MemberObject;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,11 +20,13 @@ public class MemberBlockController {
 
 	private final MemberBlockServiceImpl memberBlockService;
 
+	@Operation(summary = "차단하기", description = "특정 멤버를 차단합니다.")
+	@ApiResponses(value = {
+		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "차단 성공")})
 	@PostMapping("/member/blocks")
 	public ApiResponse<MemberBlockResponse.MemberBlockResult> block(
-		@RequestBody MemberBlockReq memberBlockReq, @MemberObject
-	Member member) {
-		return ApiResponse.onSuccess(memberBlockService.block(memberBlockReq, member));
+		@RequestBody MemberBlockReq memberBlockReq, @MemberObject Member loginMember) {
+		return ApiResponse.onSuccess(memberBlockService.block(memberBlockReq, loginMember));
 	}
 
 
--- src/main/java/com/example/dgbackend/domain/memberblock/repository/MemberBlockRepository.java
+++ src/main/java/com/example/dgbackend/domain/memberblock/repository/MemberBlockRepository.java
@@ -2,6 +2,7 @@ package com.example.dgbackend.domain.memberblock.repository;
 
 import com.example.dgbackend.domain.member.Member;
 import com.example.dgbackend.domain.memberblock.MemberBlock;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberBlockRepository extends JpaRepository<MemberBlock, Long> {

6c71f0bb736a0f9cb897af1de46f935aef63bc77
SongMinHyeok <minhyeok.dev@gmail.com>
2024-07-19 23:17:06 +0900

    Feat: 레시피 작성, 수정, 삭제 시 멤버 검증 로직 추가
    - 로그인한 멤버에 맞게 엔티티 저장 로직 추가
    - 댓글 작성자와 로그인한 멤버가 같은지 검증하는 로직 구현
    
    Related: #169

--- src/main/java/com/example/dgbackend/domain/recipe/controller/RecipeController.java
+++ src/main/java/com/example/dgbackend/domain/recipe/controller/RecipeController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,77 +29,81 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class RecipeController {
 
-    private final RecipeServiceImpl recipeServiceImpl;
-    private final RecipeScheduler recipeScheduler;
+	private final RecipeServiceImpl recipeServiceImpl;
+	private final RecipeScheduler recipeScheduler;
 
-    @Operation(summary = "모든 레시피북 조회", description = "삭제되지 않은 레시피북 목록을 조회합니다.")
-    @Parameter(name = "page", description = "페이지 번호, Query Param 입니다.", required = true, example = "0", in = ParameterIn.QUERY)
-    @GetMapping
-    public ApiResponse<RecipeResponse.RecipeResponseList> getRecipes(@RequestParam("page") int page,
-                                                                     @MemberObject Member member) {
-        return ApiResponse.onSuccess(recipeServiceImpl.getExistRecipes(page, member));
-    }
+	@Operation(summary = "모든 레시피북 조회", description = "삭제되지 않은 레시피북 목록을 조회합니다.")
+	@Parameter(name = "page", description = "페이지 번호, Query Param 입니다.", required = true, example = "0", in = ParameterIn.QUERY)
+	@GetMapping
+	public ApiResponse<RecipeResponse.RecipeResponseList> getRecipes(@RequestParam("page") int page,
+		@Parameter(hidden = true) @MemberObject Member loginMember) {
+		return ApiResponse.onSuccess(recipeServiceImpl.getExistRecipes(page, loginMember));
+	}
 
-    @Operation(summary = "레시피북 상세정보 조회", description = "특정 레시피북 정보를 조회합니다.")
-    @Parameter(name = "recipeId", description = "레시피북 Id, Path Variable 입니다.", required = true, example = "1", in = ParameterIn.PATH)
-    @GetMapping("/{recipeId}")
-    public ApiResponse<RecipeResponse> getRecipe(@PathVariable Long recipeId,
-                                                 @MemberObject Member member) {
-        return ApiResponse.onSuccess(recipeServiceImpl.getRecipeDetail(recipeId, member));
-    }
+	@Operation(summary = "레시피북 상세정보 조회", description = "특정 레시피북 정보를 조회합니다.")
+	@Parameter(name = "recipeId", description = "레시피북 Id, Path Variable 입니다.", required = true, example = "1", in = ParameterIn.PATH)
+	@GetMapping("/{recipeId}")
+	public ApiResponse<RecipeResponse> getRecipe(@PathVariable Long recipeId,
+		@Parameter(hidden = true) @MemberObject Member loginMember) {
+		return ApiResponse.onSuccess(recipeServiceImpl.getRecipeDetail(recipeId, loginMember));
+	}
 
-    @Operation(summary = "레시피북 등록", description = "레시피북을 등록합니다.")
-    @PostMapping
-    public ApiResponse<RecipeResponse> createRecipe(@MemberObject Member member,
-        @RequestBody RecipeRequest recipeRequest) {
-        return ApiResponse.onSuccess(recipeServiceImpl.createRecipe(recipeRequest, member));
-    }
+	@Operation(summary = "레시피북 등록", description = "레시피북을 등록합니다.")
+	@PostMapping
+	public ApiResponse<RecipeResponse> createRecipe(@RequestBody RecipeRequest recipeRequest,
+		@Parameter(hidden = true) @MemberObject Member loginMember) {
+		return ApiResponse.onSuccess(recipeServiceImpl.createRecipe(recipeRequest, loginMember));
+	}
 
-    @Operation(summary = "레시피북 수정", description = "레시피북을 수정합니다.")
-    @Parameter(name = "recipeId", description = "레시피북 Id, Path Variable 입니다.", required = true, example = "1", in = ParameterIn.PATH)
-    @PatchMapping("/{recipeId}")
-    public ApiResponse<RecipeResponse> updateRecipe(@PathVariable Long recipeId,
-        @RequestBody RecipeRequest recipeRequest) {
-        return ApiResponse.onSuccess(recipeServiceImpl.updateRecipe(recipeId, recipeRequest));
-    }
+	@Operation(summary = "레시피북 수정", description = "레시피북을 수정합니다.")
+	@Parameter(name = "recipeId", description = "레시피북 Id, Path Variable 입니다.", required = true, example = "1", in = ParameterIn.PATH)
+	@PatchMapping("/{recipeId}")
+	public ApiResponse<RecipeResponse> updateRecipe(@PathVariable Long recipeId,
+		@RequestBody RecipeRequest recipeRequest,
+		@Parameter(hidden = true) @MemberObject Member loginMember) {
+		return ApiResponse.onSuccess(
+			recipeServiceImpl.updateRecipe(recipeId, recipeRequest, loginMember));
+	}
 
-    @Operation(summary = "레시피북 삭제", description = "레시피북을 삭제합니다.")
-    @Parameter(name = "recipeId", description = "레시피북 Id, Path Variable 입니다.", required = true, example = "1", in = ParameterIn.PATH)
-    @DeleteMapping("/{recipeId}")
-    public ApiResponse<String> deleteRecipe(@PathVariable Long recipeId) {
-        recipeServiceImpl.deleteRecipe(recipeId);
-        return ApiResponse.onSuccess("삭제 완료");
-    }
+	@Operation(summary = "레시피북 삭제", description = "레시피북을 삭제합니다.")
+	@Parameter(name = "recipeId", description = "레시피북 Id, Path Variable 입니다.", required = true, example = "1", in = ParameterIn.PATH)
+	@DeleteMapping("/{recipeId}")
+	public ApiResponse<String> deleteRecipe(@PathVariable Long recipeId,
+		@Parameter(hidden = true) @MemberObject Member loginMember) {
+		return ApiResponse.onSuccess(recipeServiceImpl.deleteRecipe(recipeId, loginMember));
+	}
 
-    @Operation(summary = "내가 작성한 레시피북 조회", description = "특정 회원의 레시피북 목록을 조회합니다.")
-    @Parameter(name = "page", description = "내가 작성한 레시피북 목록 페이지 번호, query string 입니다.")
-    @GetMapping("/my-page")
-    public ApiResponse<RecipeResponse.RecipeMyPageList> getMyPageList(
-        @MemberObject Member member, @CheckPage @RequestParam(name= "page" ) Integer page) {
-        return ApiResponse.onSuccess(recipeServiceImpl.getRecipeMyPageList(member, page));
-    }
+	@Operation(summary = "내가 작성한 레시피북 조회", description = "특정 회원의 레시피북 목록을 조회합니다.")
+	@Parameter(name = "page", description = "내가 작성한 레시피북 목록 페이지 번호, query string 입니다.")
+	@GetMapping("/my-page")
+	public ApiResponse<RecipeResponse.RecipeMyPageList> getMyPageList(
+		@Parameter(hidden = true) @MemberObject Member loginMember,
+		@CheckPage @RequestParam(name = "page") Integer page) {
+		return ApiResponse.onSuccess(recipeServiceImpl.getRecipeMyPageList(loginMember, page));
+	}
 
-    @Operation(summary = "내가 좋아요한 레시피북 조회", description = "좋아요를 누른 레시피북 목록을 조회합니다.")
-    @Parameter(name = "page", description = "내가 좋아요한 목록 페이지 번호, query string 입니다.")
-    @GetMapping("/likes")
-    public ApiResponse<RecipeResponse.RecipeMyPageList> getLikeList(
-        @MemberObject Member member, @CheckPage @RequestParam(name = "page") Integer page) {
-        return ApiResponse.onSuccess(recipeServiceImpl.getRecipeLikeList(member, page));
-    }
+	@Operation(summary = "내가 좋아요한 레시피북 조회", description = "좋아요를 누른 레시피북 목록을 조회합니다.")
+	@Parameter(name = "page", description = "내가 좋아요한 목록 페이지 번호, query string 입니다.")
+	@GetMapping("/likes")
+	public ApiResponse<RecipeResponse.RecipeMyPageList> getLikeList(
+		@Parameter(hidden = true) @MemberObject Member loginMember,
+		@CheckPage @RequestParam(name = "page") Integer page) {
+		return ApiResponse.onSuccess(recipeServiceImpl.getRecipeLikeList(loginMember, page));
+	}
 
-    @Operation(summary = "레시피북 검색", description = "레시피북 목록을 검색합니다.")
-    @GetMapping("/search")
-    public ApiResponse<RecipeResponse.RecipeResponseList> findRecipesByKeyword(
-        @RequestParam(value = "page") Integer page,
-        @RequestParam(value = "keyword") String keyword,
-        @MemberObject Member member) {
-        return ApiResponse.onSuccess(recipeServiceImpl.findRecipesByKeyword(page, keyword, member));
-    }
+	@Operation(summary = "레시피북 검색", description = "레시피북 목록을 검색합니다.")
+	@GetMapping("/search")
+	public ApiResponse<RecipeResponse.RecipeResponseList> findRecipesByKeyword(
+		@RequestParam(value = "page") Integer page, @RequestParam(value = "keyword") String keyword,
+		@Parameter(hidden = true) @MemberObject Member loginMember) {
+		return ApiResponse.onSuccess(
+			recipeServiceImpl.findRecipesByKeyword(page, keyword, loginMember));
+	}
 
-    @Operation(summary = "메인 레시피북 조회", description = "메인에 띄울 레시피북을 조회합니다.")
-    @GetMapping("/main")
-    public ApiResponse<RecipeResponse.RecipeMainList> getMainRecipeList() {
-        return ApiResponse.onSuccess(recipeScheduler.getMainTodayRecipeList());
-    }
+	@Operation(summary = "메인 레시피북 조회", description = "메인에 띄울 레시피북을 조회합니다.")
+	@GetMapping("/main")
+	public ApiResponse<RecipeResponse.RecipeMainList> getMainRecipeList() {
+		return ApiResponse.onSuccess(recipeScheduler.getMainTodayRecipeList());
+	}
 }
 
--- src/main/java/com/example/dgbackend/domain/recipe/service/RecipeService.java
+++ src/main/java/com/example/dgbackend/domain/recipe/service/RecipeService.java
@@ -4,34 +4,34 @@ import com.example.dgbackend.domain.member.Member;
 import com.example.dgbackend.domain.recipe.Recipe;
 import com.example.dgbackend.domain.recipe.dto.RecipeRequest;
 import com.example.dgbackend.domain.recipe.dto.RecipeResponse;
-import java.util.List;
 
 public interface RecipeService {
 
-    RecipeResponse.RecipeResponseList getExistRecipes(int page, Member member);
+	RecipeResponse.RecipeResponseList getExistRecipes(int page, Member loginMember);
 
-    RecipeResponse getRecipeDetail(Long id, Member member);
+	RecipeResponse getRecipeDetail(Long id, Member member);
 
-    RecipeResponse createRecipe(RecipeRequest recipeRequest, Member member);
+	RecipeResponse createRecipe(RecipeRequest recipeRequest, Member loginMember);
 
-    RecipeResponse updateRecipe(Long id, RecipeRequest recipeRequest);
+	RecipeResponse updateRecipe(Long id, RecipeRequest recipeRequest, Member loginMember);
 
-    void deleteRecipe(Long id);
+	String deleteRecipe(Long id, Member loginMember);
 
-    //레시피 이름과 회원 이름으로 레시피 탐색
-    Recipe getRecipe(Long id);
+	//레시피 이름과 회원 이름으로 레시피 탐색
+	Recipe getRecipe(Long id);
 
-    //레시피가 삭제된 경우 예외처리
-    Recipe isDelete(Recipe recipe);
+	//레시피가 삭제된 경우 예외처리
+	Recipe isDelete(Recipe recipe);
 
-    void isAlreadyCreate(String RecipeName, String memberName);
+	void isAlreadyCreate(String RecipeName, String memberName);
 
-    RecipeResponse.RecipeMyPageList getRecipeMyPageList(Member member, Integer Page);
+	RecipeResponse.RecipeMyPageList getRecipeMyPageList(Member loginMember, Integer Page);
 
-    RecipeResponse.RecipeMyPageList getRecipeLikeList(Member member, Integer Page);
+	RecipeResponse.RecipeMyPageList getRecipeLikeList(Member loginMember, Integer Page);
 
-    RecipeResponse.RecipeResponseList findRecipesByKeyword(Integer page, String keyword, Member member);
+	RecipeResponse.RecipeResponseList findRecipesByKeyword(Integer page, String keyword,
+		Member loginMember);
 
-    RecipeResponse getRecipeDetailResponse(Recipe recipes, Member member);
+	RecipeResponse getRecipeDetailResponse(Recipe recipes, Member loginMember);
 
 }

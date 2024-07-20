d85e00f271f28d540a96292359de340806344903
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-07-20 10:12:17 +0900

    Feat: 레시피 리팩토링 및 멤버 검증 기능 구현 (#214)
    * Style: 레시피 코드 스타일 수정
    
    Related: #169
    
    * Feat: Add MemberValidator class for validating member actions
    
    Related: #169
    
    * Feat: Add _INVALID_MEMBER error status
    
    Related: #169
    
    * Feat: _INVALID_MEMBER 수정
    
    Related: #169
    
    * Feat: 댓글 작성, 수정, 삭제 시 멤버 검증 로직 추가
    
    - 로그인한 멤버에 맞게 엔티티 저장 로직 추가
    - 댓글 작성자와 로그인한 멤버가 같은지 검증하는 로직 구현
    
    Related: #169
    
    * Feat: 레시피 작성, 수정, 삭제 시 멤버 검증 로직 추가
    
    - 로그인한 멤버에 맞게 엔티티 저장 로직 추가
    - 댓글 작성자와 로그인한 멤버가 같은지 검증하는 로직 구현
    
    Related: #169
    
    * Docs: @Schema 어노테이션의 해시태그 리스트 예제 값 수정
    
    Related: #169

--- src/main/java/com/example/dgbackend/domain/recipecomment/controller/RecipeCommentController.java
+++ src/main/java/com/example/dgbackend/domain/recipecomment/controller/RecipeCommentController.java
@@ -1,7 +1,6 @@
 package com.example.dgbackend.domain.recipecomment.controller;
 
 import com.example.dgbackend.domain.member.Member;
-import com.example.dgbackend.domain.member.repository.MemberRepository;
 import com.example.dgbackend.domain.recipecomment.dto.RecipeCommentRequest;
 import com.example.dgbackend.domain.recipecomment.dto.RecipeCommentResponse;
 import com.example.dgbackend.domain.recipecomment.dto.RecipeCommentVO;
@@ -13,9 +12,15 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "레시피북 댓글 API")
 @RestController
@@ -25,8 +30,6 @@ public class RecipeCommentController {
 
 	private final RecipeCommentServiceImpl recipeCommentService;
 
-	private final MemberRepository memberRepository;
-
 	@Operation(summary = "레시피북 댓글 조회", description = "특정 레시피북의 댓글을 조회합니다.")
 	@Parameter(name = "recipeId", description = "레시피북 Id, Path Variable 입니다.", required = true, example = "1", in = ParameterIn.PATH)
 	@Parameter(name = "page", description = "페이지 번호, Query Param 입니다.", required = true, example = "0", in = ParameterIn.QUERY)
@@ -34,7 +37,8 @@ public class RecipeCommentController {
 	public ApiResponse<RecipeCommentResponse.RecipeCommentResponseList> getRecipeComments(
 		@PathVariable Long recipeId, @RequestParam("page") int page,
 		@Parameter(hidden = true) @MemberObject Member loginMember) {
-		return ApiResponse.onSuccess(recipeCommentService.getRecipeComment(recipeId, page, loginMember));
+		return ApiResponse.onSuccess(
+			recipeCommentService.getRecipeComment(recipeId, page, loginMember));
 	}
 
 	@Operation(summary = "레시피북 댓글 등록", description = "레시피북 댓글을 등록합니다.")
@@ -42,26 +46,29 @@ public class RecipeCommentController {
 	@PostMapping("/{recipeId}")
 	public ApiResponse<RecipeCommentResponse> saveRecipeComment(@PathVariable Long recipeId,
 		@RequestBody RecipeCommentRequest.Post recipeCommentRequest,
-		@MemberObject Member member) {
+		@Parameter(hidden = true) @MemberObject Member loginMember) {
 		RecipeCommentVO paramVO = RecipeCommentVO.of(recipeCommentRequest, recipeId,
-			member.getName());
-		return ApiResponse.onSuccess(recipeCommentService.saveRecipeComment(paramVO));
+			loginMember.getName());
+		return ApiResponse.onSuccess(recipeCommentService.saveRecipeComment(paramVO, loginMember));
 	}
 
 	@Operation(summary = "레시피북 댓글 수정", description = "레시피북 댓글을 수정합니다.")
 	@PatchMapping
 	public ApiResponse<RecipeCommentResponse> updateRecipeComment(
-		@RequestBody RecipeCommentRequest.Patch recipeCommentRequest) {
+		@RequestBody RecipeCommentRequest.Patch recipeCommentRequest,
+		@Parameter(hidden = true) @MemberObject Member loginMember) {
 		return ApiResponse.onSuccess(
-			recipeCommentService.updateRecipeComment(recipeCommentRequest));
+			recipeCommentService.updateRecipeComment(recipeCommentRequest, loginMember));
 	}
 
 	@Operation(summary = "레시피북 댓글 삭제", description = "레시피북 댓글을 삭제합니다.")
 	@Parameter(name = "recipeCommentId", description = "레시피북 댓글 Id, Query Param 입니다.", required = true, example = "1", in = ParameterIn.QUERY)
 	@DeleteMapping
 	public ApiResponse<RecipeCommentResponse> deleteRecipeComment(
-		@RequestParam Long recipeCommentId) {
-		return ApiResponse.onSuccess(recipeCommentService.deleteRecipeComment(recipeCommentId));
+		@RequestParam Long recipeCommentId,
+		@Parameter(hidden = true) @MemberObject Member loginMember) {
+		return ApiResponse.onSuccess(
+			recipeCommentService.deleteRecipeComment(recipeCommentId, loginMember));
 	}
 
 }
--- src/main/java/com/example/dgbackend/domain/recipecomment/service/RecipeCommentServiceImpl.java
+++ src/main/java/com/example/dgbackend/domain/recipecomment/service/RecipeCommentServiceImpl.java
@@ -1,8 +1,9 @@
 package com.example.dgbackend.domain.recipecomment.service;
 
+import static com.example.dgbackend.global.common.MemberValidator.isMatch;
+
 import com.example.dgbackend.domain.enums.State;
 import com.example.dgbackend.domain.member.Member;
-import com.example.dgbackend.domain.member.service.MemberService;
 import com.example.dgbackend.domain.recipe.Recipe;
 import com.example.dgbackend.domain.recipe.repository.RecipeRepository;
 import com.example.dgbackend.domain.recipecomment.RecipeComment;
@@ -13,97 +14,108 @@ import com.example.dgbackend.domain.recipecomment.repository.RecipeCommentReposi
 import com.example.dgbackend.global.common.response.code.status.ErrorStatus;
 import com.example.dgbackend.global.exception.ApiException;
 import jakarta.transaction.Transactional;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class RecipeCommentServiceImpl implements RecipeCommentService {
 
-    private final RecipeCommentRepository recipeCommentRepository;
-    private final MemberService memberService;
-    private final RecipeRepository recipeRepository;
+	private final RecipeCommentRepository recipeCommentRepository;
+	private final RecipeRepository recipeRepository;
 
-    @Override
-    public RecipeCommentResponse.RecipeCommentResponseList getRecipeComment(Long recipeId, int page, Member loginMember) {
+	@Override
+	public RecipeCommentResponse.RecipeCommentResponseList getRecipeComment(Long recipeId, int page,
+		Member loginMember) {
 
-        Recipe recipe = recipeRepository.findById(recipeId).orElseThrow(() -> new ApiException(ErrorStatus._EMPTY_RECIPE));
+		Recipe recipe = recipeRepository.findById(recipeId)
+			.orElseThrow(() -> new ApiException(ErrorStatus._EMPTY_RECIPE));
 
-        Pageable pageable = Pageable.ofSize(10).withPage(page);
+		Pageable pageable = Pageable.ofSize(10).withPage(page);
 
-        Page<RecipeComment> recipeCommentPage = recipeCommentRepository.findByRecipeAndParentCommentIsNullAndStateIsTrueOrReported(recipe, pageable, loginMember.getId());
+		Page<RecipeComment> recipeCommentPage = recipeCommentRepository.findByRecipeAndParentCommentIsNullAndStateIsTrueOrReported(
+			recipe, pageable, loginMember.getId());
 
-        return RecipeCommentResponse.toResponseList(recipeCommentPage);
-    }
+		return RecipeCommentResponse.toResponseList(recipeCommentPage);
+	}
 
-    @Override
-    @Transactional
-    public RecipeCommentResponse saveRecipeComment(RecipeCommentVO paramVO) {
-        RecipeComment save = recipeCommentRepository.save(getEntity(paramVO));
+	@Override
+	@Transactional
+	public RecipeCommentResponse saveRecipeComment(RecipeCommentVO paramVO, Member loginMember) {
+		RecipeComment save = recipeCommentRepository.save(getEntity(paramVO, loginMember));
 
-        //댓글 수 증가
-        save.getRecipe().changeCommentCount(true);
-        return RecipeCommentResponse.toResponse(save);
-    }
+		//댓글 수 증가
+		save.getRecipe().changeCommentCount(true);
+		return RecipeCommentResponse.toResponse(save);
+	}
 
-    @Override
-    public RecipeComment getEntity(RecipeCommentVO paramVO) {
-        Member member = memberService.findMemberByName(paramVO.getMemberName());
-        Recipe recipe = recipeRepository.findById(paramVO.getRecipeId()).orElseThrow(() -> new ApiException(ErrorStatus._EMPTY_RECIPE));
-        String content = paramVO.getContent();
+	@Override
+	public RecipeComment getEntity(RecipeCommentVO paramVO, Member loginMember) {
+		Recipe recipe = recipeRepository.findById(paramVO.getRecipeId())
+			.orElseThrow(() -> new ApiException(ErrorStatus._EMPTY_RECIPE));
+		String content = paramVO.getContent();
 
-        return Optional.ofNullable(paramVO.getParentId())
+		return Optional.ofNullable(paramVO.getParentId())
 
-                //대댓글 일 때
-                .filter(parentId -> parentId != 0)
-                .map(parentId -> RecipeCommentRequest.toEntity(member, recipe, content, getParentEntityById(parentId)))
+			//대댓글 일 때
+			.filter(parentId -> parentId != 0)
+			.map(parentId -> RecipeCommentRequest.toEntity(loginMember, recipe, content,
+				getParentEntityById(parentId)))
 
-                //댓글 일 때
-                .orElse(RecipeCommentRequest.toEntity(member, recipe, content, null));
-    }
+			//댓글 일 때
+			.orElse(RecipeCommentRequest.toEntity(loginMember, recipe, content, null));
+	}
 
-    @Override
-    public RecipeComment getParentEntityById(Long parentId) {
-        RecipeComment ParentRecipeComment = getEntityById(parentId);
+	@Override
+	public RecipeComment getParentEntityById(Long parentId) {
+		RecipeComment ParentRecipeComment = getEntityById(parentId);
 
-        //부모의 부모 댓글이 존재할 경우
-        if (ParentRecipeComment.getParentComment() != null) {
-            throw new ApiException(ErrorStatus._OVER_DEPTH_RECIPE_COMMENT);
-        }
+		//부모의 부모 댓글이 존재할 경우
+		if (ParentRecipeComment.getParentComment() != null) {
+			throw new ApiException(ErrorStatus._OVER_DEPTH_RECIPE_COMMENT);
+		}
 
-        return ParentRecipeComment;
-    }
+		return ParentRecipeComment;
+	}
 
-    @Override
-    public RecipeComment getEntityById(Long id) {
-        return recipeCommentRepository.findById(id)
-                .orElseThrow(() -> new ApiException(ErrorStatus._EMPTY_RECIPE_COMMENT));
-    }
+	@Override
+	public RecipeComment getEntityById(Long id) {
+		return recipeCommentRepository.findById(id)
+			.orElseThrow(() -> new ApiException(ErrorStatus._EMPTY_RECIPE_COMMENT));
+	}
 
-    @Override
-    @Transactional
-    public RecipeCommentResponse updateRecipeComment(RecipeCommentRequest.Patch requestDTO) {
-        RecipeComment recipeComment = getEntityById(requestDTO.getRecipeCommentId()).update(requestDTO.getContent());
-        return RecipeCommentResponse.toResponse(recipeComment);
-    }
+	@Override
+	@Transactional
+	public RecipeCommentResponse updateRecipeComment(RecipeCommentRequest.Patch requestDTO,
+		Member loginMember) {
 
-    @Override
-    @Transactional
-    public RecipeCommentResponse deleteRecipeComment(Long recipeCommentId) {
-        RecipeComment entityById = getEntityById(recipeCommentId);
+		RecipeComment recipeComment = getEntityById(requestDTO.getRecipeCommentId());
+		if (isMatch(loginMember, recipeComment.getMember())) {
+			return RecipeCommentResponse.toResponse(recipeComment.update(requestDTO.getContent()));
+		} else {
+			throw new ApiException(ErrorStatus._INVALID_MEMBER);
+		}
+	}
 
-        if (entityById.getState().equals(State.FALSE)) {
-            throw new ApiException(ErrorStatus._Already_DELETE_RECIPE_COMMENT);
-        }
+	@Override
+	@Transactional
+	public RecipeCommentResponse deleteRecipeComment(Long recipeCommentId, Member loginMember) {
+		RecipeComment recipeComment = getEntityById(recipeCommentId);
 
-        RecipeComment recipeComment = entityById.delete();
+		if (recipeComment.getState().equals(State.FALSE)) {
+			throw new ApiException(ErrorStatus._Already_DELETE_RECIPE_COMMENT);
+		}
 
-        //대댓글도 삭제
-        recipeComment.getChildCommentList().forEach(RecipeComment::delete);
-        return RecipeCommentResponse.toResponse(recipeComment);
-    }
+		if (isMatch(loginMember, recipeComment.getMember())) {
+			//대댓글도 삭제 -> 대댓글 구현시 수정
+			//recipeComment.getChildCommentList().forEach(RecipeComment::delete);
+			return RecipeCommentResponse.toResponse(recipeComment.delete());
+		} else {
+			throw new ApiException(ErrorStatus._INVALID_MEMBER);
+		}
+
+	}
 }

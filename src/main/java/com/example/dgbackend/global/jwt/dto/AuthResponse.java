97ea042550703052ea75bd49d8bbed656005dc9e
Lee seohyeon 이서현 <101540728+Blue-hyeon@users.noreply.github.com>
2024-07-24 17:43:33 +0900

    feat: 새 유저/기존 유저 구분 API (#221)

--- src/main/java/com/example/dgbackend/global/jwt/dto/AuthResponse.java
+++ src/main/java/com/example/dgbackend/global/jwt/dto/AuthResponse.java
@@ -6,26 +6,43 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Builder
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
 public class AuthResponse {
 
-    private String provider;
-    private String nickName;
-    private Long memberId;
-    private boolean newMember;
-    private LocalDateTime createdAt;
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class AuthResult {
+        private String provider;
+        private String nickName;
+        private Long memberId;
+        private boolean newMember;
+        private LocalDateTime createdAt;
+    }
 
-    public static AuthResponse toAuthResponse(String provider, String nickName, boolean newMember, Long memberId) {
+    public static AuthResult toAuthResult(String provider, String nickName, boolean newMember, Long memberId) {
 
-        return AuthResponse.builder()
+        return AuthResult.builder()
             .provider(provider)
             .nickName(nickName)
             .memberId(memberId)
             .newMember(newMember)
             .createdAt(LocalDateTime.now())
             .build();
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class IsSignedUpResult {
+        private Boolean isSignedUp;
+    }
+
+    public static IsSignedUpResult toIsSignedUpResult(Boolean isSignedUp) {
+
+        return IsSignedUpResult.builder()
+                .isSignedUp(isSignedUp)
+                .build();
     }
 }

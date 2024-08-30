34467a0c51ce974d1ad976133a6cd91fa99d893d
SongMinHyeok <minhyeok.dev@gmail.com>
2024-08-30 17:08:14 +0900

    Change: 레시피북 이미지 업로드 방식 수정
    - 기존 Combination Image 업로드 방식과 동일하게 수정 및 기존 로직 삭제
    
    Related: #239

--- src/main/java/com/example/dgbackend/domain/recipeimage/service/RecipeImageService.java
+++ src/main/java/com/example/dgbackend/domain/recipeimage/service/RecipeImageService.java
@@ -1,31 +1,27 @@
 package com.example.dgbackend.domain.recipeimage.service;
 
 import com.example.dgbackend.domain.recipe.Recipe;
-import com.example.dgbackend.domain.recipe.repository.RecipeRepository;
-import com.example.dgbackend.domain.recipe.service.RecipeService;
 import com.example.dgbackend.domain.recipeimage.RecipeImage;
-import com.example.dgbackend.domain.recipeimage.dto.RecipeImageRequest;
 import com.example.dgbackend.domain.recipeimage.dto.RecipeImageResponse;
-import com.example.dgbackend.domain.recipeimage.dto.RecipeImageVO;
 import com.example.dgbackend.domain.recipeimage.repository.RecipeImageRepository;
 import com.example.dgbackend.global.common.response.code.status.ErrorStatus;
 import com.example.dgbackend.global.exception.ApiException;
 import com.example.dgbackend.global.s3.S3Service;
 import com.example.dgbackend.global.s3.dto.S3Result;
-import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 @Slf4j
 public class RecipeImageService {
 
     private final RecipeImageRepository recipeImageRepository;
-    private final RecipeRepository recipeRepository;
 
     private final S3Service s3Service;
 
@@ -33,30 +29,6 @@ public class RecipeImageService {
         return recipeImageRepository.findAllByRecipeId(recipeId).stream()
             .map(RecipeImage::getImageUrl)
             .toList();
-    }
-
-    @Transactional
-    public RecipeImageResponse createRecipeImage(RecipeImageVO.FileVO request) {
-        Recipe recipe = recipeRepository.findById(request.getRecipeId())
-            .orElseThrow(() -> new ApiException(ErrorStatus._EMPTY_RECIPE));
-
-        //파일 없을 시 예외처리
-        List<MultipartFile> multipartFiles = validFileList(request.getFileList());
-
-        //S3에 이미지 업로드하고 URL 받아오기
-        List<String> imageURLs = s3Service.uploadFile(multipartFiles).stream()
-            .map(S3Result::getImgUrl)
-            .toList();
-
-        //RecipeImage(url 리스트, recipe) 엔티티 생성
-        List<RecipeImage> recipeImages = imageURLs.stream()
-            .map(imageURL -> RecipeImageRequest.toEntity(recipe, imageURL))
-            .toList();
-
-        //RecipeImage 엔티티 모두 저장
-        recipeImageRepository.saveAll(recipeImages);
-
-        return RecipeImageResponse.toURLResponse(recipe);
     }
 
     //파일 없을 시 예외처리
@@ -67,31 +39,6 @@ public class RecipeImageService {
         }
 
         return request;
-    }
-
-    @Transactional
-    public RecipeImageResponse updateRecipeImage(RecipeImageVO.FileVO request) {
-        Recipe recipe = recipeRepository.findById(request.getRecipeId())
-            .orElseThrow(() -> new ApiException(ErrorStatus._EMPTY_RECIPE));
-
-        //새로운 이미지 업로드
-        if (request.getFileList() != null && !request.getFileList().isEmpty()) {
-            createRecipeImage(request);
-        }
-
-        List<String> deleteFileUrlList = request.getDeleteFileUrlList();
-
-        //삭제 요청 이미지 삭제
-        if (deleteFileUrlList != null && !deleteFileUrlList.isEmpty()) {
-            deleteFileUrlList.forEach(
-                deleteFileUrl -> {
-                    deleteRecipeImage(deleteFileUrl);
-                    s3Service.deleteFile(deleteFileUrl);
-                }
-            );
-        }
-
-        return RecipeImageResponse.toURLResponse(recipe);
     }
 
     //recipeImageRepository에서 imageUrl로 조회해서 있으면 삭제하고 s3에서도 삭제
@@ -128,6 +75,65 @@ public class RecipeImageService {
 
     private List<RecipeImage> loadImage(Long recipeId) {
         return recipeImageRepository.findAllByRecipeId(recipeId);
+    }
+
+    public RecipeImageResponse.RecipeImageResult uploadRecipeImage(
+        List<MultipartFile> requestFiles) {
+
+        List<MultipartFile> multipartFiles = validFileList(requestFiles);
+
+        // S3에 이미지 업로드
+        List<String> imageUrls = s3Service.uploadFile(multipartFiles)
+            .stream()
+            .map(S3Result::getImgUrl)
+            .toList();
+
+        return RecipeImageResponse.toRecipeImageResult(imageUrls);
+
+    }
+
+    public void updateRecipeImage(Recipe recipe, List<String> recipeImageList) {
+
+        // 1. 기존의 이미지 파일 조회
+        List<String> existRecipeImageUrls = recipeImageRepository.findAllByRecipeId(recipe.getId())
+            .stream()
+            .map(RecipeImage::getImageUrl)
+            .toList();
+
+        // 2. 수정하면서 없어진 이미지를 S3에서 삭제하기
+        removeCancelledRecipeImage(recipeImageList, existRecipeImageUrls);
+
+        // 3. 새로 추가된 이미지 RecipeImage에 저장하기 (S3에 저장하기)
+        addNewRecipeImage(recipe, recipeImageList, existRecipeImageUrls);
+
+    }
+
+    public void removeCancelledRecipeImage(List<String> recipeImageList,
+        List<String> existRecipeImageUrls) {
+
+        List<String> delImageUrls = existRecipeImageUrls.stream()
+            .filter(existImage -> !recipeImageList.contains(existImage))
+            .toList();
+
+        delImageUrls.forEach(recipeImageRepository::deleteByImageUrl);
+        delImageUrls.forEach(s3Service::deleteFile);
+
+    }
+
+    public void addNewRecipeImage(Recipe recipe, List<String> recipeImageList,
+        List<String> existRecipeImageUrls) {
+
+        List<String> addImageUrls = recipeImageList.stream()
+            .filter(request -> !existRecipeImageUrls.contains(request))
+            .toList();
+
+        List<RecipeImage> recipeImages = addImageUrls.stream()
+            .map(image -> RecipeImage.builder()
+                .recipe(recipe)
+                .imageUrl(image)
+                .build())
+            .toList();
+        recipeImages.forEach(recipe::addRecipeImage);
     }
 
 

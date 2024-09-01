638f8ff940a7e45643d772908670adfff261de2c
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-01 14:22:48 +0900

    Docs: 오늘의 조합 좋아요 등록 스웨거 설명 추가
    Related: #249

--- src/main/java/com/example/dgbackend/domain/combinationlike/controller/CombinationLikeController.java
+++ src/main/java/com/example/dgbackend/domain/combinationlike/controller/CombinationLikeController.java
@@ -5,6 +5,8 @@ import com.example.dgbackend.domain.combinationlike.service.CombinationLikeQuery
 import com.example.dgbackend.domain.member.Member;
 import com.example.dgbackend.global.common.response.ApiResponse;
 import com.example.dgbackend.global.jwt.annotation.MemberObject;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +21,8 @@ public class CombinationLikeController {
     private final CombinationLikeQueryServiceImpl combinationLikeQueryServiceImpl;
 
 
+    @Operation(summary = "오늘의 조합 좋아요 누르기", description = "오늘의 조합 좋아요를 등록합니다. 기존에 존재하면 State를 변경합니다.")
+    @Parameter(name = "combinationId", description = "오늘의 조합 Id, Path Variable 입니다.", required = true)
     @PostMapping("/combination-likes/{combinationId}")
     public ApiResponse<Boolean> updateCombinationLike(@MemberObject Member member,
         @PathVariable(value = "combinationId") Long combinationId) {

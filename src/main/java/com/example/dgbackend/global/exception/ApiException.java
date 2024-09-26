0e2ad149df65f821b42172a2a44b6ab4d5702482
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-27 01:19:18 +0900

    Feat: ApiException 발생 시 Error Log 출력
    Related: #267

--- src/main/java/com/example/dgbackend/global/exception/ApiException.java
+++ src/main/java/com/example/dgbackend/global/exception/ApiException.java
@@ -2,14 +2,21 @@ package com.example.dgbackend.global.exception;
 
 import com.example.dgbackend.global.common.response.code.ErrorReasonDto;
 import com.example.dgbackend.global.common.response.code.status.ErrorStatus;
+import lombok.extern.log4j.Log4j2;
 
-
+@Log4j2
 public class ApiException extends RuntimeException {
 
+    private final String errorCode;
+    private String errorMessage;
     private final ErrorStatus errorStatus;
 
     public ApiException(ErrorStatus errorStatus) {
         super(errorStatus.getMessage());
+        errorCode = errorStatus.getCode();
+        errorMessage = errorStatus.getMessage();
+        log.error("In ApiException errorCode: {}, errorMessage: {}, Status: {}", errorCode,
+            errorMessage, errorStatus);
         this.errorStatus = errorStatus;
     }
 

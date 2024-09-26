05acf95021f05c5101a8853613bc0e524d00df53
SongMinHyeok <minhyeok.dev@gmail.com>
2024-09-27 00:59:55 +0900

    Feat: LogWebConfig 설정을 통해 Interceptor 활성화
    Related: #267

---
+++ src/main/java/com/example/dgbackend/global/config/log/LogWebConfig.java
@@ -0,0 +1,21 @@
+package com.example.dgbackend.global.config.log;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class LogWebConfig implements WebMvcConfigurer {
+
+    private final Interceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+            .addPathPatterns("/**");
+    }
+
+
+}
